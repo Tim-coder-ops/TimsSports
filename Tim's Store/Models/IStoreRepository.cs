0d039df651261cb347266d766eb90020411e5b6b
Tim-coder-ops <timweigel63@gmail.com>
2020-09-11 21:00:08 -0700

    Completed step 1

---
+++ Tim's Store/Models/IStoreRepository.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq;
+
+namespace OutDoorProducts.Models {
+    public interface IStoreRepository {
+
+        IQueryable<Product> Products { get; }
+    }
+}
---
+++ Tim's Store/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using OutDoorProducts.Models;
+
+namespace OutDoorProducts.Models.ViewModels {
+
+    public class ProductsListViewModel {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}

31898abe237d70262f7b6ce14fdb44bf21efeaca
Tim-coder-ops <timweigel63@gmail.com>
2020-09-23 19:39:09 -0700

    Added Checkout

---
+++ ECommerceProject/Controllers/OrderController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ECommerceProject.Models;
+namespace ECommerceProject.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}

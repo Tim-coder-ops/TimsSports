f109a5563fcda41dbf2a4a41d739fe9248cccdb5
Tim-coder-ops <timweigel63@gmail.com>
2020-09-29 10:56:45 -0700

    Finished all Blazer components

--- ECommerceProject/Migrations/20200929171057_Shipped.Designer.cs
+++ ECommerceProject/Migrations/20200929171057_Shipped.Designer.cs
@@ -4,14 +4,16 @@ using ECommerceProject.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ECommerceProject.Migrations
 {
     [DbContext(typeof(StoreDbContext))]
-    partial class StoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200929171057_Shipped")]
+    partial class Shipped
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

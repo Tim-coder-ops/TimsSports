b220246ce86d57b8addd66e70d340396acaee3ca
Tim-coder-ops <timweigel63@gmail.com>
2020-10-22 22:15:38 -0700

    Added Docker

--- ECommerceProject/Startup.cs
+++ ECommerceProject/Startup.cs
@@ -43,6 +43,16 @@ namespace ECommerceProject
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsProduction())
+            {
+                app.UseExceptionHandler("/error");
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
+
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
